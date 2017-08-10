a0606880e3e3cf928a6e2a41e5bff829bf81220e
Raphael Thome <raphaelthome@raphaels-macbook-pro.local>
2017-08-10 02:29:43 -0300

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Raphael Thome  on 8/10/17.
+//  Copyright © 2017 Raphael Thome . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
